12d80ab94e425d0bde517c99015b185f98445e54
Andrei Tsypilnikov <andrei.tsypilnikov92@gmail.com>
2024-01-06 17:34:34 +0400

    Homework 1 is done

--- homework01/main.cpp
+++ homework01/main.cpp
@@ -2,6 +2,6 @@
 #include "version.h"
 
 int main() {
-    std::cout << "build " << version() << std::endl;
+    std::cout << "Build '" << version() << "'" << std::endl;
     std::cout << "Hello, World!" << std::endl;
 }
